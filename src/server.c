906b97fd585d5dbfc3afee35193d099303070bc2
Giraudux <giraudux@gmail.com>
2016-01-31 21:55:55 +0100

    add posix thread support

--- src/server.c
+++ src/server.c
@@ -3,16 +3,30 @@ Serveur à lancer avant le client
 ------------------------------------------------*/
 #include <stdlib.h>
 #include <stdio.h>
-#include <linux/types.h> 	/* pour les sockets */
+#include <linux/types.h>     /* pour les sockets */
 #include <sys/socket.h>
-#include <netdb.h> 		/* pour hostent, servent */
-#include <string.h> 		/* pour bcopy, ... */  
+#include <unistd.h>
+#include <netdb.h>         /* pour hostent, servent */
+#include <string.h>         /* pour bcopy, ... */  
+#include <pthread.h>
+
 #define TAILLE_MAX_NOM 256
 
 typedef struct sockaddr sockaddr;
 typedef struct sockaddr_in sockaddr_in;
 typedef struct hostent hostent;
 typedef struct servent servent;
+
+typedef struct {
+    pthread_t thread;
+    int socket;
+    char name[32];/*define macro*/
+} player_t;
+
+typedef struct {
+    player_t players[2];/*define macro*/
+    int players_size;
+} room_t;
 
 /*------------------------------------------------------*/
 void renvoi (int sock) {
@@ -21,7 +35,7 @@ void renvoi (int sock) {
     int longueur;
    
     if ((longueur = read(sock, buffer, sizeof(buffer))) <= 0) 
-    	return;
+        return;
     
     printf("message lu : %s \n", buffer);
     
@@ -46,32 +60,41 @@ void renvoi (int sock) {
 }
 /*------------------------------------------------------*/
 
+void * otel_connect(void * player) {
+    renvoi(((player_t*)player)->socket);
+    close(((player_t*)player)->socket);
+    free(player);
+
+    return NULL;
+}
+
 /*------------------------------------------------------*/
-main(int argc, char **argv) {
+int main(int argc, char **argv) {
   
-    int 		socket_descriptor, 		/* descripteur de socket */
-			nouv_socket_descriptor, 	/* [nouveau] descripteur de socket */
-			longueur_adresse_courante; 	/* longueur d'adresse courante d'un client */
-    sockaddr_in 	adresse_locale, 		/* structure d'adresse locale*/
-			adresse_client_courant; 	/* adresse client courant */
-    hostent*		ptr_hote; 			/* les infos recuperees sur la machine hote */
-    servent*		ptr_service; 			/* les infos recuperees sur le service de la machine */
-    char 		machine[TAILLE_MAX_NOM+1]; 	/* nom de la machine locale */
+    int socket_descriptor,         /* descripteur de socket */
+        nouv_socket_descriptor,     /* [nouveau] descripteur de socket */
+        longueur_adresse_courante,
+        status;     /* longueur d'adresse courante d'un client */
+    sockaddr_in adresse_locale,         /* structure d'adresse locale*/
+                adresse_client_courant;     /* adresse client courant */
+    hostent * ptr_hote;             /* les infos recuperees sur la machine hote */
+    servent * ptr_service;             /* les infos recuperees sur le service de la machine */
+    char machine[TAILLE_MAX_NOM+1];     /* nom de la machine locale */
     
-    gethostname(machine,TAILLE_MAX_NOM);		/* recuperation du nom de la machine */
+    gethostname(machine,TAILLE_MAX_NOM);        /* recuperation du nom de la machine */
     
     /* recuperation de la structure d'adresse en utilisant le nom */
     if ((ptr_hote = gethostbyname(machine)) == NULL) {
-		perror("erreur : impossible de trouver le serveur a partir de son nom.");
-		exit(1);
+        perror("erreur : impossible de trouver le serveur a partir de son nom.");
+        exit(1);
     }
     
-    /* initialisation de la structure adresse_locale avec les infos recuperees */			
+    /* initialisation de la structure adresse_locale avec les infos recuperees */            
     
     /* copie de ptr_hote vers adresse_locale */
     bcopy((char*)ptr_hote->h_addr, (char*)&adresse_locale.sin_addr, ptr_hote->h_length);
-    adresse_locale.sin_family		= ptr_hote->h_addrtype; 	/* ou AF_INET */
-    adresse_locale.sin_addr.s_addr	= INADDR_ANY; 			/* ou AF_INET */
+    adresse_locale.sin_family        = ptr_hote->h_addrtype;     /* ou AF_INET */
+    adresse_locale.sin_addr.s_addr    = INADDR_ANY;             /* ou AF_INET */
 
     /* 2 facons de definir le service que l'on va utiliser a distance */
     /* (commenter l'une ou l'autre des solutions) */
@@ -80,8 +103,8 @@ main(int argc, char **argv) {
     /* SOLUTION 1 : utiliser un service existant, par ex. "irc" */
     /*
     if ((ptr_service = getservbyname("irc","tcp")) == NULL) {
-		perror("erreur : impossible de recuperer le numero de port du service desire.");
-		exit(1);
+        perror("erreur : impossible de recuperer le numero de port du service desire.");
+        exit(1);
     }
     adresse_locale.sin_port = htons(ptr_service->s_port);
     */
@@ -91,18 +114,18 @@ main(int argc, char **argv) {
     /*-----------------------------------------------------------*/
     
     printf("numero de port pour la connexion au serveur : %d \n", 
-		   ntohs(adresse_locale.sin_port) /*ntohs(ptr_service->s_port)*/);
+           ntohs(adresse_locale.sin_port) /*ntohs(ptr_service->s_port)*/);
     
     /* creation de la socket */
     if ((socket_descriptor = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-		perror("erreur : impossible de creer la socket de connexion avec le client.");
-		exit(1);
+        perror("erreur : impossible de creer la socket de connexion avec le client.");
+        exit(1);
     }
 
     /* association du socket socket_descriptor à la structure d'adresse adresse_locale */
     if ((bind(socket_descriptor, (sockaddr*)(&adresse_locale), sizeof(adresse_locale))) < 0) {
-		perror("erreur : impossible de lier la socket a l'adresse de connexion.");
-		exit(1);
+        perror("erreur : impossible de lier la socket a l'adresse de connexion.");
+        exit(1);
     }
     
     /* initialisation de la file d'ecoute */
@@ -110,26 +133,48 @@ main(int argc, char **argv) {
 
     /* attente des connexions et traitement des donnees recues */
     for(;;) {
+
+        player_t * player = malloc(sizeof(player_t));
+        if(player == NULL) {
+            perror("erreur : malloc");
+            exit(1);
+        }
     
-		longueur_adresse_courante = sizeof(adresse_client_courant);
-		
-		/* adresse_client_courant sera renseigné par accept via les infos du connect */
-		if ((nouv_socket_descriptor = 
-			accept(socket_descriptor, 
-			       (sockaddr*)(&adresse_client_courant),
-			       &longueur_adresse_courante))
-			 < 0) {
-			perror("erreur : impossible d'accepter la connexion avec le client.");
-			exit(1);
-		}
-		
-		/* traitement du message */
-		printf("reception d'un message.\n");
-		
-		renvoi(nouv_socket_descriptor);
-						
-		close(nouv_socket_descriptor);
-		
+        longueur_adresse_courante = sizeof(adresse_client_courant);
+        
+        /* adresse_client_courant sera renseigné par accept via les infos du connect */
+        /*if ((nouv_socket_descriptor =
+            accept(socket_descriptor, 
+                   (sockaddr*)(&adresse_client_courant),
+                   &longueur_adresse_courante))
+             < 0) {
+            perror("erreur : impossible d'accepter la connexion avec le client.");
+            exit(1);
+        }*/
+
+        if ((player->socket =
+                    accept(socket_descriptor,
+                           (sockaddr*)(&adresse_client_courant),
+                           &longueur_adresse_courante))
+                     < 0) {
+                    perror("erreur : impossible d'accepter la connexion avec le client.");
+                    exit(1);
+                }
+        
+        /* traitement du message */
+        printf("reception d'un message.\n");
+
+        status = pthread_create(&(player->thread), NULL, otel_connect, player);
+        if(status) {
+            perror("erreur : pthread_create");
+            exit(1);
+        }
+        
+        /*renvoi(nouv_socket_descriptor);*/
+
+        /*close(nouv_socket_descriptor);*/
+        
     }
-    
+
+    return 0;
 }
